1c9c777002214e624dc0229433fe767a563c1ea6
Matt Benavente <mbenavente77@gmail.com>
2018-11-13 14:26:23 -0600

    Added Redux DevTools

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import Hello from './containers/Hello';
 import { enthusiasm } from './reducers/index';
 // import { StoreState } from './types/index';
 
 // @TODO Why does this have <StoreState> typing in th TypescriptReactStarter readme?
-const store = createStore(enthusiasm, {
-    enthusiasmLevel: 1,
-    languageName: 'TypeScript',
-});
+const store = createStore(
+    enthusiasm, 
+    {
+      enthusiasmLevel: 1,
+      languageName: 'TypeScript',
+    },
+    composeWithDevTools()
+);
 
 ReactDOM.render(
   <Provider store={store}>
